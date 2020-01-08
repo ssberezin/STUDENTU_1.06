55859731f581a17e4a31d478cc74d7b787029639
ssberezin <zakazatkr@yandex.ru>
2020-01-09 00:30:48 +0200

    8/1/20 - пилим интерфейс окна сравнения контактніх данных

---
+++ STUDENTU_1.06/Views/EditOrderWindows/ContactsWindows/AddContactsWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows;
+
+
+namespace STUDENTU_1._06.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AddContactsWindow.xaml
+    /// </summary>
+    public partial class AddContactsWindow : Window
+    {
+        public AddContactsWindow(object DataContext)
+        {
+            InitializeComponent();
+            this.DataContext = DataContext;
+        }
+        
+    }
+}
