dd5570185951a11edacac7405a0133a2f7841960
Sergey <flpberezinss@gmail.com>
2021-03-06 23:19:12 +0200

    06.03.21 - пил ветки авторизации пользователя
    оформлено окно отображения пользователей, нужно пилить логику под него.

---
+++ STUDENTU_1.06/Views/PersoneOperations/UserOperations/UsersInformationWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace STUDENTU_1._06.Views.PersoneOperations.UserOperations
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersInformationWindow.xaml
+    /// </summary>
+    public partial class UsersInformationWindow : Window
+    {
+        public UsersInformationWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new _Authorisation();
+        }
+    }
+}
